4e7f2d151dea00edb8662223e18ea9cf5cdbb58a
Halil Coşgun <cosgun.halil@gmail.com>
2018-02-21 17:28:03 +0300

    architecture workouts

---
+++ DailyChestSystem/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    public DataManager _dataManager;
+    public TimeManager _timeManager;
+    public NtpServerConnectionManager _ntpServerConnectionManager;
+    public ChestManager _chestManager;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+	}
+}
--- DailyChestSystem/Assets/Scripts/TimeManager.cs
+++ DailyChestSystem/Assets/Scripts/TimeManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Timer {
+public class TimeManager : MonoBehaviour {
 
+	
 }
